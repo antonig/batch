1b644d29a70b654dd32b5388c89c914a25f6c3fd
Antoni Gual <antongualvia@gmail.com>
2022-09-28 13:48:24 +0200

    Update primes_TAOCP.cmd

--- primes_TAOCP.cmd
+++ primes_TAOCP.cmd
@@ -1,40 +1,41 @@
-:: prime generator using a sorted multiples list stored in a priority queue
-:: uses the environment as the priority queue, limited to maxprime 100000
-:: from a problem in Knuth's TAOCP
-:: Be warned it will crash when the environment size reaches 64K!!
-
+:: prime table using rotating multiples stored in environment
+:: maxprime=100000 deoe'nt crash the environement in my PC. Adjust for yours
 @echo off
 setlocal enabledelayedexpansion 
-set path=
 mode con cols=90
 set @25=-10
-set /a num=7,inc1=4,cnt=0,inc2=0,num1=0, maxprime=65535
-set lin=    0:       2       3       5
+set /a num=7,inc1=4,cnt=0,inc2=0,num1=0, maxprime=100000
+set lin=    0:
+call:line 2 & call:line 3 & call:line 5
 
 :nextnum
 if defined @%num% ( 
   for %%i in (!@%num%!) do (
-   
-    set /a n2=num1*num1
-    if !n2! leq %maxprime%  (  if %%i lss 0  (set /a num1=%num%-%%i,"inc2=-%%i<<1") else (set /a num1=%num%+%%i,"inc2=-(%%i>>1)")
-    for %%j in (@!num1!) do  set %%j=!inc2! !%%j! )
-    )
+    if %%i lss 0  (set /a num1=%num%-%%i,"inc2=-%%i<<1") else (set /a num1=%num%+%%i,"inc2=-(%%i>>1)")
+    
+  if !num1! leq %maxprime% ( for %%j in (@!num1!) do  set %%j=!inc2! !%%j! )
+   )
   set @%num%=
-)else (
-  set num2=       %num%
-  set lin=%lin%!num2:~-8!
-  if not "!lin:~80,1!" equ "" (
-    echo !lin! 
-    set /a cnt+=10
-    set cnt1=    !cnt!
-    set lin=!cnt1:~-5!:
-  )
-  
+) else (
+  call :line %num%
+  set /a num1= num * num 
   if %inc1% equ 4 (set/a "inc2=num<<2") else (set /a "inc2=-(num<<1)")
-  set @!num1!=!inc2! 
+  if !num1! leq %maxprime%   set @!num1!=!inc2! 
 )
 set /a num+=inc1, inc1=6-inc1
 if %num% lss %maxprime% goto nextnum
+
 echo %lin%
-pause
-exit /b
+goto:eof
+
+ 
+
+:line        formats output in 10 right aligned columns
+set num2=       %1
+set lin=%lin%%num2:~-8%
+set /a cnt+=1,res1=(cnt%%10)
+if %res1% neq 0 goto:eof
+echo %lin% 
+set cnt1=    %cnt%
+set lin=%cnt1:~-5%:
+goto:eof 
